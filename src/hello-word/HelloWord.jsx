c059a9d66c90b49da2afb715eabbd3eb3b95b9ac
fikri-mtwoo <ahmad.fikrinew2801@gmail.com>
2024-12-22 19:35:34 +0700

    javascript di JSX

--- src/hello-word/HelloWord.jsx
+++ src/hello-word/HelloWord.jsx
@@ -8,9 +8,13 @@ export default function HelloWord(){
 }
 
 function HeadingHelloWord() {
+    const data = "Hello Word";
     return (
         <>
-            <h1>Hello Word</h1>
+            <h1 style={{
+                color : "orange",
+                backgroundColor : "black"
+            }}>{data}</h1>
         </>
     )
 }
@@ -26,3 +30,22 @@ function ParagrafHelloWord() {
 //componen return dari function jsx
 //dalam function idealnya atau best practices memiliki satu function yang sesuai dengan nama file
 //tapi bisa juga dalam satu file mempunya banyak function yang mengembalikan tampilan
+
+//kenapa menggunakan jsx dalam react
+//karena tampilan web sekarang lebih interaktif dan sering nya konten HTML ditampilkan berdasarkan logika aplikasi
+//website dibangun menggunakan HTML, CSS, Javascript
+// HTML sebagai kerangka
+// CSS sebagai tampilan
+// javascript sebagai behavior atau logic
+
+//aturan dalama jsx
+// - component hanya dapat mengembalikan satu element jika ingin mengembalikan element lebih dari satu makan harus dibungkus dalam parent element atau fragment kosong
+// wajib menutup semua tag element
+// attribut dalam element menggunakan aturan penulisan variable dalam javascript seperti camel case
+
+//kurung kurawal dalam JSX
+//javascript dalam JSX ditulis dalam tanga kurung kurawal {}
+//atau untuk nilai atribut pada tag element misal <img src={location}/>
+
+//kurung kurawal double dalam JSX
+//java script object sebagai nilai attribut element
