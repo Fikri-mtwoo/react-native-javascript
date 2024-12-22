c950e119c7439e706e78845a98b5480dffa0a42f
fikri-mtwoo <ahmad.fikrinew2801@gmail.com>
2024-12-22 19:16:41 +0700

    component

--- src/hello-word/HelloWord.jsx
+++ src/hello-word/HelloWord.jsx
@@ -1,8 +1,28 @@
 export default function HelloWord(){
     return (
         <>
-            <h1>Hello Word</h1>
-            <p>Belajar React JS</p>
+            <HeadingHelloWord/>
+            <ParagrafHelloWord/>
         </>
     )
 }
+
+function HeadingHelloWord() {
+    return (
+        <>
+            <h1>Hello Word</h1>
+        </>
+    )
+}
+
+function ParagrafHelloWord() {
+    return (
+        <>
+            <p>Belajar React JS Native</p>
+        </>
+    )
+}
+
+//componen return dari function jsx
+//dalam function idealnya atau best practices memiliki satu function yang sesuai dengan nama file
+//tapi bisa juga dalam satu file mempunya banyak function yang mengembalikan tampilan
